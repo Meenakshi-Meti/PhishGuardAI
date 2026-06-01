54a204670d6ab3cb7d17efe0121abeddaaea9c16
Meenakshi <meenakshi28633@gmail.com>
2026-06-01 12:14:38 +0530

    Created Navbar and Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero(){
+    return (
+        <section>
+            <h1>Detect Phishing Websites Before They Steal Your Data</h1>
+
+            <p>AI-Powered phishing detection system for websites, emails and files.</p>
+            <button>Get Started</button>
+        </section>
+    );
+}
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+        <nav class="navbar">
+            <h2>PhishGuardAI</h2>
+
+            <ul>
+                <li>Home</li>
+                <li>Features</li>
+                <li>Dashboard</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
