bc9a19ab22252baab40037959eb926fd124683ad
Meenakshi Singh Tanwar <meenakshi28633@gmail.com>
2026-06-02 22:54:47 +0530

    Added Scanner and Features Components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,21 @@
 function Hero(){
     return (
         <section class="hero">
-            <h1>Detect Phishing Websites Before They Steal Your Data</h1>
 
-            <p>AI-Powered phishing detection system for websites, emails and files.</p>
-            <button>Get Started</button>
+            <div className="hero-left">
+                <span className ="badge">AI-Powered Threat Detection</span>
+
+                <h1>Detect Phishing Websites Before They Steal</h1>
+
+                <p>AI-Powered phishing detection system for websites, emails and files.</p>
+            <div className ="hero-buttons">
+            <button>Start Free Scan</button>
+            <button>Watch Demo</button>
+            </div>
+            </div>
+            <div className="hero-right">
+                <shield3D />
+            </div>
         </section>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
 function Navbar(){
     return(
         <nav class="navbar">
-            <h2>PhishGuardAI</h2>
-
+            <div className ="logo">
+                <h2>🛡️PhishGuardAI</h2>
+            </div>
             <ul>
                 <li>Home</li>
                 <li>Features</li>
                 <li>Dashboard</li>
+                <li>Pricing</li>
+                <li>About</li>
                 <li>Contact</li>
             </ul>
+
+            <div className="nav-button">
+            <button className="login-btn">Log In</button>
+
+            <button className ="signup-btn">Sign Up</button>
+            
+            </div>
         </nav>
     );
 }
